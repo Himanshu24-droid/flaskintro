080f7d90f384760f1fefe6969e659e3f8226b127
Himanshu Lanjewar <himanshulanjewar24@gmail.com>
2022-01-06 15:56:17 +0530

    updated __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,22 +6,18 @@ from flask import Flask
 basedir = Path(__file__).resolve().parent
 
 def create_app(test_config=None):
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-        SQALCHEMY_DATABASE_URI=os.getenv("DATABASE_URL", f"sqlite:///{Path(basedir).joinpath(os.path.join(app.instance_path, 'flaskr.sqlite'))}")
+        SQLALCHEMY_DATABASE_URI=os.getenv("DATABASE_URL", f"sqlite:///{Path(basedir).joinpath(os.path.join(app.instance_path, 'flaskr.sqlite'))}")
     )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
