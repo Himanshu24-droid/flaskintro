8705ba1502c971416700229a8569eafba7919957
Himanshu Lanjewar <himanshulanjewar24@gmail.com>
2022-01-10 16:04:59 +0530

    added exception

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -10,17 +10,21 @@ bp=Blueprint('blog', __name__)
 
 @bp.route('/')
 def index():
-    db=get_db()
-    posts=db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    comments=db.execute(
-        'SELECT c.id, comment, created, user_name, post_id '
-        ' FROM comment c JOIN user u ON c.user_name = u.username'
-        ' ORDER BY created ASC'
-    ).fetchall()
+    try:
+        db=get_db()
+        posts=db.execute(
+            'SELECT p.id, title, body, created, author_id, username'
+            ' FROM post p JOIN user u ON p.author_id = u.id'
+            ' ORDER BY created DESC'
+        ).fetchall()
+        comments=db.execute(
+            'SELECT c.id, comment, created, user_name, post_id '
+            ' FROM comment c JOIN user u ON c.user_name = u.username'
+            ' ORDER BY created ASC'
+        ).fetchall()
+    except sqlite3.OperationalError:
+        flash('You have no database.')
+
     return render_template('blog/index.html', posts=posts,comments=comments)
 
 @bp.route('/create', methods=('GET', 'POST'))
